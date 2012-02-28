3054c2a57b46bbdf0637729558b143042e8468a4
Thibaut Barrère <thibaut.barrere@gmail.com>
2012-02-28 19:27:26 +0100

    Raise unless the config is in database.yml

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,7 @@ require File.dirname(__FILE__) + '/../lib/adapter_extensions'
 
 raise "Missing required DB environment variable" unless ENV['DB']
 configs = YAML.load_file('test/config/database.yml')
+raise "Configuration #{ENV['DB']} not in database.yml!"
 ActiveRecord::Base.configurations = configs
 ActiveRecord::Base.establish_connection(ENV['DB'])
 
