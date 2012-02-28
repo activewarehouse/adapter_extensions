d4b6c4ed60e68bcdc0832e7416f48e9d1f4a8fe5
Thibaut Barrère <thibaut.barrere@gmail.com>
2012-02-28 19:03:24 +0100

    Until I can have a before(:all), use this central place for database recreate/setup.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,20 @@ require 'pp'
 
 require File.dirname(__FILE__) + '/../lib/adapter_extensions'
 
-db = ENV['DB'] ||= 'mysql'
-require "connection/#{db}/connection"
+############## TODO - move this to adapter_test.rb "setup before all" ##############################
+
+raise "Missing required DB environment variable" unless ENV['DB']
+configs = YAML.load_file('test/config/database.yml')
+ActiveRecord::Base.configurations = configs
+ActiveRecord::Base.establish_connection(ENV['DB'])
+
+connection = ActiveRecord::Base.connection
+connection.recreate_database(connection.current_database)
+connection.reconnect!
+
+def normalize_adapter_name(adapter_name)
+  adapter_name == 'mysql2' ? 'mysql' : adapter_name
+end
+
+sql_setup = IO.read(File.dirname(__FILE__) + "/config/databases/#{normalize_adapter_name(ENV['DB'])}_setup.sql")
+sql_setup.split(';').each { |line| connection.execute(line) }
