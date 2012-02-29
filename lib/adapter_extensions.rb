5a3455073e9c9b914be9d7c3ef38adc300be78e4
Thibaut Barrère <thibaut.barrere@gmail.com>
2012-02-29 16:18:47 +0100

    Avoid stack level too deep on 1.8.7.

--- lib/adapter_extensions.rb
+++ lib/adapter_extensions.rb
@@ -2,11 +2,15 @@ require 'adapter_extensions/base'
 
 class ActiveRecord::Base
   class << self
-    def establish_connection_with_adapter_extensions(*args)
-      establish_connection_without_adapter_extensions(*args)
-      ActiveSupport.run_load_hooks(:active_record_connection_established, connection_pool)
+    # for 1.8.7, modules loaded more than once that use alias_method_chain will 
+    # cause stack level too deep errors - make sure we avoid this
+    unless self.instance_methods.include?('establish_connection_without_adapter_extensions')
+      def establish_connection_with_adapter_extensions(*args)
+        establish_connection_without_adapter_extensions(*args)
+        ActiveSupport.run_load_hooks(:active_record_connection_established, connection_pool)
+      end
+      alias_method_chain :establish_connection, :adapter_extensions
     end
-    alias_method_chain :establish_connection, :adapter_extensions
   end
 end
 
