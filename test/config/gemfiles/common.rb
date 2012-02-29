2a9db4b38336f0731f6d7037570b589c9a01bc7b
Thibaut Barrère <thibaut.barrere@gmail.com>
2012-02-29 15:08:21 +0100

    Use mysql2 < 0.3 for rails < 3.1

--- test/config/gemfiles/common.rb
+++ test/config/gemfiles/common.rb
@@ -3,9 +3,13 @@ def declare_gems(activerecord_version)
 
   gem 'activerecord', activerecord_version
 
-  # use our own fork for bulk load support until issue fixed:
-  # https://github.com/brianmario/mysql2/pull/242
-  gem 'mysql2', :git => 'https://github.com/activewarehouse/mysql2.git'
+  if activerecord_version < '3.1'
+    gem 'mysql2', '< 0.3'
+  else
+    # use our own fork for bulk load support until issue fixed:
+    # https://github.com/brianmario/mysql2/pull/242
+    gem 'mysql2', :git => 'https://github.com/activewarehouse/mysql2.git'
+  end
 
   gem 'mysql'
 
