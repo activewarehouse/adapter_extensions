e0519340eaeb9272f66c6c90819b57025e46609e
Tom Phillips <tdphillipsjr@gmail.com>
2021-05-08 22:32:08 -0400

    remove alias_method_chain

--- lib/adapter_extensions.rb
+++ lib/adapter_extensions.rb
@@ -9,7 +9,8 @@ class ActiveRecord::Base
         establish_connection_without_adapter_extensions(*args)
         ActiveSupport.run_load_hooks(:active_record_connection_established, connection_pool)
       end
-      alias_method_chain :establish_connection, :adapter_extensions
+      alias_method :establish_connection, :establish_connection_with_adapter_extensions
+      alias_method :establish_connection_without_adapter_extensions, :establish_connection
     end
   end
 end
